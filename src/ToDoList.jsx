25a1b6bb5630bc9f8dcc3e5e45ef547ca594f33d
vvsafwan <vvsafwan2002@gmail.com>
2024-02-09 14:43:24 +0530

    first commit

---
+++ src/ToDoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+const ToDoList = (props) => {
+
+  return (
+    <>
+        <div className='todo_style'>
+            <i className='fa fa-times' aria-hidden='true' onClick={() => {
+                props.onSelect(props.id)
+            }} />
+            <li> {props.text} </li>
+        </div>
+    </>
+  )
+}
+
+export default ToDoList 
